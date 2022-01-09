797da46fa10bfa9723fbb62c5a0f461bedcd721f
Tobias <sirianni.gf@gmail.com>
2022-01-09 18:59:52 -0300

    Setting hashed .js with clean: true

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
+        filename: '[name].[contenthash].js',
+        clean: true,
     }
 }
