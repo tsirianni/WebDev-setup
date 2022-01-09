e8c03d34faabc6b2bedaa96edfa2e87383f0a341
Tobias <sirianni.gf@gmail.com>
2022-01-09 18:55:39 -0300

    Basic settings webpack

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WebDev-setup
+
+`npm init -y` and delete main in **package.jason**
+
+`npm i -D webpack webpack-cli` install dependencies for webpack
+
+Create src/index.js
+
+Create **webpack.config.js** with:
+```
+
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // or Production
+
+    entry: {
+        main: path.resolve(__dirname, 'src/app.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    }
+}
